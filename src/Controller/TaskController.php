bae2151b952e01699f9500a0237e0388d9e14358
CamileGhastine <camileghastine@hotmail.com>
2022-10-25 17:00:21 +0200

    category : entity Category, Sendd categories to view

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,15 +11,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class TaskController extends AbstractController
 {
     public function __construct(private TaskRepository $taskRepo) {
-
     }
 
     #[Route('/tasks', name: 'app_tasks')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepo): Response
     {
+
+        $categories = $categoryRepo->findAll();
+
         $tasks = $this->taskRepo->findAll();
+
         return $this->render('task/index.html.twig', [
             'tasks' => $tasks,
+            'categories' =>$categories
         ]);
     }
 }
--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\TaskRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -29,6 +31,14 @@ class Task
     #[ORM\ManyToOne(inversedBy: 'tasks')]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $user = null;
+
+    #[ORM\ManyToMany(targetEntity: Category::class, mappedBy: 'tasks')]
+    private Collection $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -91,6 +101,33 @@ class Task
     public function setUser(?User $user): self
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Category>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories->add($category);
+            $category->addTask($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        if ($this->categories->removeElement($category)) {
+            $category->removeTask($this);
+        }
 
         return $this;
     }
