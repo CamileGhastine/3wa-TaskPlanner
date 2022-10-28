1ab0470f532988260f4a8261407262b777bedc43
CamileGhastine <camileghastine@hotmail.com>
2022-10-28 12:24:03 +0200

    createTask: taskType configuration and add categories in form

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -2,25 +2,55 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Task;
+use App\Repository\CategoryRepository;
+use App\Repository\TaskRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TaskType extends AbstractType
 {
+    public function __construct(private CategoryRepository $repo) {
+
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('title', TextType::class, [
-                'label' => 'Titre'
+                'label' => 'Titre',
+                'required' => true,
+                'attr' => [
+                    'class' => 'titi',
+                    'id' => 'tata'
+                ]
             ])
-            ->add('description')
-            ->add('expiratedAt')
-            ->add('Enregistrer', SubmitType::class, [
+            ->add('description', TextareaType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Ecrire votre texte ici ...'
+                ]
             ])
+            ->add('expiratedAt', DateType::class)
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+                'label' => 'Catégories'
+                ])
+            ->add('Enregistrer', SubmitType::class)
         ;
     }
 
